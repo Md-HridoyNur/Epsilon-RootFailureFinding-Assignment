85fe11cb8d91d5311c9fb43e40bdb7d931795ab5
MD RIFAT MOJUMDER ROWNAK <mojumderrownak17@gmail.com>
2025-09-17 22:51:31 +0600

    add

--- Codes/FalsePosition.cpp
+++ Codes/FalsePosition.cpp
@@ -1,1 +1,14 @@
 #include<iostream>
+#include<iomanip>
+#include<cmath>
+#include<vector>
+
+using namespace std;
+
+double f(const vector<double>& coeffs,double x){
+    int n = coeffs.size()-1;
+    for (int i=0;i<=n;i++){
+        result += coeffs[i]*pow(x,n-1);
+    }
+    return result ;
+}
