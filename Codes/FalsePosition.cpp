c30d49f1653f6863a73ba1fa24e4c4d6972cdcd0
MD RIFAT MOJUMDER ROWNAK <mojumderrownak17@gmail.com>
2025-09-17 23:05:52 +0600

    add

--- Codes/FalsePosition.cpp
+++ Codes/FalsePosition.cpp
@@ -12,3 +12,16 @@ double f(const vector<double>& coeffs,double x){
     }
     return result ;
 }
+
+double maxrootestimate ( const vector<double>& coeffs){
+    int n = coeffs.size()-1;
+    if (n<2) return 1.0;
+    double a_n = coeffs[0];
+    double a_n1 = coeffs[1];
+    double a_n2 = coeffs[2];
+    double discrim = ( a_n1 / a_n)*(a_n1/a_n)-2*(a_n2/a_n);
+    if ( discrim <0 ) discrim = 0;
+    return sqrt ( discrim);
+}
+
+
