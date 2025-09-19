b6686d7b13e5365ccb4750676382ee934a4c8ded
Jannat Kamal Sathi <jannatkamalsathi@gmail.com>
2025-09-20 00:53:09 +0600

    Update RootFindingFailure.cpp

--- Codes/RootFindingFailure.cpp
+++ Codes/RootFindingFailure.cpp
@@ -9,4 +9,8 @@ constexpr double TOL = 1e-10;
 
 double f_cuberoot(double x) { return cbrt(x); }
 ouble df_cuberoot(double x) {
-    
+    if (x == 0.0) return 0.0;
+    double t = cbrt(x);
+    return (1.0 / 3.0) / (t * t);
+}
+
