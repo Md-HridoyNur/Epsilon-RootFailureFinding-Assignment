bd563bf61053325594bb7d7540c687e80933fa93
Md HridoyNur <mdhridoynur5300@gmail.com>
2025-09-20 16:54:15 +0600

    push

--- Codes/RootFindingFailure.cpp
+++ Codes/RootFindingFailure.cpp
@@ -7,7 +7,7 @@ constexpr int MAXIT = 200;
 constexpr double TOL = 1e-10;
 
 double f_cuberoot(double x) { return cbrt(x); }
-ouble df_cuberoot(double x) {
+double df_cuberoot(double x) {
     if (x == 0.0) return 0.0;
     double t = cbrt(x);
     return (1.0 / 3.0) / (t * t);
